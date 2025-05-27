4a52b4aca1af81078c21ee25f31a30c2ce74eb83
Richiess1 <20235894@esen.edu.sv>
2025-05-26 18:49:48 -0600

    Settings controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SettingsController } from './settings/settings.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, SettingsController],
   providers: [AppService],
 })
 export class AppModule {}
