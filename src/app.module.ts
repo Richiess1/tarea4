caef2f307f5b9c357f534afed1a283199a5c8442
astridayala <astriayala06@gmail.com>
2025-05-26 19:03:37 -0600

    creando carpetas

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SettingsController } from './settings/settings.controller';
+import { SettingsService } from './settings/settings.service';
 
 @Module({
   imports: [],
   controllers: [AppController, SettingsController],
-  providers: [AppService],
+  providers: [AppService, SettingsService],
 })
 export class AppModule {}
